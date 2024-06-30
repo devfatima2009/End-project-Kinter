aa7610e4083246eb137b07e3092893bcfb8e4237
devfatima2009 <154680274+devfatima2009@users.noreply.github.com>
2024-06-30 12:30:01 +0400

    header-top over

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySlider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".mySlide", {
+    loop:true,
+    spaceBetween:30,
+    slidesPerView: 5,
+  }); 
